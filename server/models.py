e856f00002e032e4e664672c57e547a468d1e8aa
ajackwere <austinewere59@gmail.com>
2023-10-01 13:20:49 +0300

    Models created

---
+++ server/models.py
@@ -0,0 +1,46 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(250), nullable=False)
+
+    pizzas = db.relationship(
+        'Pizza', secondary='restaurant_pizzas', backref='restaurants')
+
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+    serialize_rules = ('-restaurant_pizzas.pizza',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    ingredients = db.Column(db.String(500), nullable=False)
+
+    restaurants = db.relationship(
+        'Restaurant', secondary='restaurant_pizzas', backref='pizzas')
+
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizzas'
+    serialize_rules = ('-restaurant.pizzas', '-pizza.restaurants',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(
+        db.Integer, db.ForeignKey(restaurants.id), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey(
+        'pizzas.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False,
+                      check_constraint='price >= 1 AND price <= 30')
+
+    restaurant = db.relationship('Restaurant', backref=db.backref(
+        'restaurant_pizzas', cascade='all, delete-orphan'))
+    pizza = db.relationship('Pizza', backref=db.backref(
+        'restaurant_pizzas', cascade='all, delete-orphan'))
